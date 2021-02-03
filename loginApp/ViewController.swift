fd4707cf80c15d318362222856a6def8f1601849
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2021-02-03 22:56:30 +0300

    First code setup is done in ViewController

--- loginApp/ViewController.swift
+++ loginApp/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func forgotUserNameButtonPressed() {
+        showAlert(with: "Opps!", and: "Your name is User")
+    }
+    
+    @IBAction func forgotPasswordButtonPressed() {
+        showAlert(with: "Opps!", and: "Your password is Password")
+    }
+    
 
 }
 
+// MARK: - Alert Controller
+extension ViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        // cюда же еще действие при ошибке дописать
+        present(alert, animated: true)
+    }
+}
---
+++ loginApp/WelcomeScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeScreenViewController.swift
+//  loginApp
+//
+//  Created by o11ev on 03.02.2021.
+//
+
+import UIKit
+
+class WelcomeScreenViewController: UIViewController {
+    
+    
+    @IBOutlet var welcomeUserLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
